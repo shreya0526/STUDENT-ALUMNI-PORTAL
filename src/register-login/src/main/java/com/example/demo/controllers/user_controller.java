04ba45cac5cbab6aeca2b42c676d7089e9a6dcea
yatendra <yatendragoyal5@gmail.com>
2025-07-23 09:56:58 +0530

    register login form

---
+++ src/register-login/src/main/java/com/example/demo/controllers/user_controller.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+import com.example.demo.services.user_service;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/user")
+public class user_controller {
+	@Autowired
+	user_service user_serv;
+	
+	@GetMapping("/all")
+	public List<User> getall(){
+		return user_serv.getAll();
+	}
+	
+}

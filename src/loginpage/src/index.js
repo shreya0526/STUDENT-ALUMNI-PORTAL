c6f0b6862d70f2e39a36b6403e9d72cdd54cf039
SHREYA SINGH <Shreya Singh>
2025-07-24 17:56:21 +0530

    Register added siuccessfully

--- src/loginpage/src/index.js
+++ src/loginpage/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RegisterForm from './register'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RegisterForm/>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/register-login/src/main/java/com/example/demo/controllers/RoleController.java
+++ src/register-login/src/main/java/com/example/demo/controllers/RoleController.java
@@ -3,12 +3,16 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entities.Role;
 import com.example.demo.services.RoleService;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 @RequestMapping("/role")

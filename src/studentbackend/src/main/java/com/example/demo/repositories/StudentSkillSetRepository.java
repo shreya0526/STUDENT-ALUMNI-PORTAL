a3d1f18d6205dce8f63d8bb38b44e64ffb2c07b6
Nandita0901 <nanditalambadi09@gmail.com>
2025-08-01 18:59:22 +0530

    repositories added successfully

---
+++ src/studentbackend/src/main/java/com/example/demo/repositories/StudentSkillSetRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.StudentSkillSet;
+
+public interface StudentSkillSetRepository extends JpaRepository<StudentSkillSet, Integer> {
+
+}

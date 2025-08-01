70037717ae84dc769a333b6c33c929d826e0be01
Nandita0901 <nanditalambadi09@gmail.com>
2025-08-01 12:47:04 +0530

    added student and city successfully

---
+++ src/studentbackend/src/main/java/com/example/demo/entities/City.java
@@ -0,0 +1,28 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="city")
+public class City {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int city_id;
+	String city_name;
+	
+	
+	
+}
---
+++ src/studentbackend/src/main/java/com/example/demo/entities/Student.java
@@ -0,0 +1,42 @@
+package com.example.demo.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="student")
+public class Student {
+     
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.IDENTITY)
+	 int student_id;
+	 
+	 @JsonIgnoreProperties("student")
+	 @OneToOne
+	 @JoinColumn(name="user_id")
+	  User user  ;
+	 
+	 @JsonIgnoreProperties("student")
+	 @ManyToOne(cascade=CascadeType.ALL)
+	 @JoinColumn(name="college_id")
+	 College college;
+	 
+	 
+}

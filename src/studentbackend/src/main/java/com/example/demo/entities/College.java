a3d0a33989458083f3128a440df93738170d692e
Nandita0901 <nanditalambadi09@gmail.com>
2025-08-01 17:48:29 +0530

    added college successfully

--- src/studentbackend/src/main/java/com/example/demo/entities/College.java
+++ src/studentbackend/src/main/java/com/example/demo/entities/College.java
@@ -1,25 +1,14 @@
 package com.example.demo.entities;
 
-import jakarta.persistence.Entity;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CascadeType;
-<<<<<<< HEAD
-=======
 import jakarta.persistence.Column;
->>>>>>> 9c69e0ffee538c1afd4e95c78385d704174721d1
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-<<<<<<< HEAD
-import jakarta.persistence.ManyToOne;
-=======
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
->>>>>>> 9c69e0ffee538c1afd4e95c78385d704174721d1
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -38,13 +27,13 @@ public class College {
 	int college_id;
 	
 	@Column(name = "college_name", nullable = false)
-    private String collegeName;
+    String collegeName;
 
     // Foreign key to City table
     @ManyToOne
     @JoinColumn(name = "city_id", nullable = false)
     @JsonIgnoreProperties("colleges") // Prevent infinite recursion
-    private City city;
+    City city;
 	
 	
 	

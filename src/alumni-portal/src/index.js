bc519dcb6ff715de7a50d797b4a520d843c3ff57
Syed Waseem Ul Hasan <waseemhasan999@gmail.com>
2025-08-06 15:07:01 +0530

    alumni dashboard

---
+++ src/alumni-portal/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import AlumniDashboard from './components/AlumniDashboard'; // Adjust path as needed
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AlumniDashboard />
+  </React.StrictMode>
+);
+
+
+
